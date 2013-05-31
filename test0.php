789feb5d5b0c2c2ac44443af6aa3ee7c5093c0fb
Huang Feng <306761352@qq.com>
2013-05-31 12:53:15 +0800

    a

--- test0.php
+++ test0.php
@@ -12,9 +12,83 @@ Kint::trace();
 // http://blog.kent-chiu.com/blog/2011/10/17/ascii_class_diagram/
 // UML（统一建模语言）
 /*
++--------+
+| Client |
++--------+
+ +--------+       +---------------------+ *
+ | Client |------>| Component           |<-----------------------------+
+ +--------+       +---------------------+                              |
+                  | Operation()         |                              |
+                  | Add(Component)      |                              |
+                  | Remove(Component)   |                              |
+                  | GetChild(int)       |                              |
+                  +---------------------+                              |
+                            #                                          |
+                            |                                          |
+                 +----------+--------------+                           |
+                 |                         |                           |
+          +------+------+       +----------+----------+  children      |
+          | Leaf        |       | Composite           |O---------------+
+          +-------------+       +---------------------+
+          | Operation() |       | Operation()         |
+          +-------------+       | Add(Component)      |
+                                | Remove(Component)   |
+                                | GetChild(int)       |
+                                +---------------------+
+
+
+
+class Pastor{
+  private   $address;
+  protected $hobby;
+  public    $name = 'Tom';
+  public    $gender;
+
+  public function __construct($para1, $para2){
+    $this->address = '';
+    $this->hobby   = array('music', 'dance');
+    $this->name    = 'Hehuan';
+    $this->gender  = 'female';
+  }
+
+  public function preach($para3){
+    echo 'We should love God' . $para3;
+  }
+
+  public static function pray(){
+    return 'God please forgive me';
+  }
+
+  protected function write(){
+    return 'I am writing';
+  }
+
+  private function makelove(){
+    return 'I am making love';
+  }
+}
+
++------------------------------------+
+| Paster                             |
++------------------------------------+
+| + $name : String [Tom]             |
+| + $gender                          |
+| - $address                         |
+| # $hobby                           |
++------------------------------------+
+| + <<create>> ($para1, $para2)      |
+| + preach($para3) : void            |
+| + <<static>> pray() : String       |
+| # write() : String                 |
+| - makelove() : String              |
++------------------------------------+
+
+
+
 类名
 属性
 方法
+
 属性
 <可见性> <属性名称> : <数据类型> [=默认值]
 
@@ -30,25 +104,106 @@ Kint::trace();
 构造函数用<<create>>表示
 析构函数用<<destroy>>表示
 
-
-
-
+-------------------------------------------------------------------------------------------------------------------
 */
 
+
 class Pastor{
-  public $name;
-  public $gender;
+  private   $address;
+  protected $hobby;
+  public    $name = 'Tom';
+  public    $gender;
+
+  public function __construct($para1, $para2){
+    $this->address = '';
+    $this->hobby   = array('music', 'dance');
+    $this->name    = 'Hehuan';
+    $this->gender  = 'female';
+  }
 
   public function preach(){
     echo 'We should love God';
   }
+
+  public static function pray(){
+    echo 'God please forgive me';
+  }
+
+  protected function write(){
+    echo 'I am writing';
+  }
+
+  private function makelove(){
+    echo 'I am making love';
+  }
 }
 
-$a = new ReflectionClass('Pastor');
-var_dump($a);die;
+$test1 = new ReflectionClass('Pastor');
+d($test1);die;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 echo '<hr />';
-
 echo '<pre>';
 $diagram = '
  +--------+       +---------------------+ *
